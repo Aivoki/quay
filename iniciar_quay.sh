9ca5f31cb0a6e0db30e0e28bc9701b6f5834ab23
Luis Henrique Braz Sobrinho <luis.sobrinho@inep.gov.br>
2020-03-13 10:18:13 -0300

    Modificando nome do composer e adicionando script para inicialização

---
+++ iniciar_quay.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+
+function stop(){
+	echo -e "Parando containers já iniciados"
+	docker-compose -f ./docker-compose.yaml -f ./initial_config.yaml -f ./clair.yaml down
+}
+
+function start(){
+	echo -e "Iniciando configuração do Quay Registry para Primeira instalação"
+	docker-compose -f ./docker-compose.yaml -f clair.yaml $1 up -d
+}
+
+function primeiraVez() {
+
+	stop
+
+	echo -e "Ajustando pasta do postgres"
+	rm -rf ./postgres/data/.gitkeep
+
+	echo -e "Ajustando pasta do mysql"
+	chmod 777 ./mysql -R
+	rm -rf ./mysql/data/*
+
+	start "-f ./initial_config.yaml"
+}
+
+function configurado(){
+	
+	stop
+
+	start
+}
+
+main(){
+	clear
+	echo -e "Digite uma opção"
+	echo -e "\n1 -\tIniciar primeira instalação do Quay Registry"
+	echo -e "2 -\tSomente iniciar Quay Registry já configurado"
+	echo -e "3 -\tParar todos os containers"
+	while [[ true ]]; do
+		read opcao
+		echo $pwd
+		case $opcao in
+			1) primeiraVez
+				break
+			;;
+			2) configurado
+				break
+			;;
+			3) stop
+				break
+			;;
+			*) echo -e "Opção inválida"
+			;;
+		esac
+	done
+}
+
+main
